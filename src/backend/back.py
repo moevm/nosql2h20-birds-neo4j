386004fae70a4a24add5069640a9a159b9580643
LosevMikhail <mikhail.losev.1998@gmail.com>
2020-11-15 14:08:02 +0300

    Some requests got implemented now

--- src/backend/back.py
+++ src/backend/back.py
@@ -14,6 +14,15 @@ class HelloWorldExample:
             greeting = session.write_transaction(self._create_and_return_greeting, message)
             print(greeting)
 
+    def getSpecies(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._get_all_species)
+            return result
+
+    def createSpec(self, name):
+        with self.driver.session() as session:
+            result = session.write_transaction(self._create_species, name)
+
     @staticmethod
     def _create_and_return_greeting(tx, message):
         result = tx.run("CREATE (a:Greeting) "
@@ -21,8 +30,24 @@ class HelloWorldExample:
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
 
+    # That returns a list of all the species represented
+    @staticmethod
+    def _get_all_species(tx):
+        req = '''MATCH (n:Species)
+                 RETURN n.name as name'''
+        result = tx.run(req)  # A dictionary or something
+        return [record["name"] for record in result]  # Make namelist of it
+
+    @staticmethod
+    def _create_species(tx, name):
+        req = '''CREATE (a:Species)
+                 SET a.name = $name '''
+        result = tx.run(req, name=name)
+        return result.single()
+
 
 if __name__ == "__main__":
     greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "password")
-    greeter.print_greeting("hello, world")
+    # greeter.print_greeting("hello, world")
+    print(greeter.getSpecies())
     greeter.close()
