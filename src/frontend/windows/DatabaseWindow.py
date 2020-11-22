f84fb4bb4f5df845ffac79192e69f61509d25dbe
Falvram <wolflammen0@gmail.com>
2020-11-22 15:02:21 +0300

    Adding Import/Export buttons

--- src/frontend/windows/DatabaseWindow.py
+++ src/frontend/windows/DatabaseWindow.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget
 from src.frontend.widgets.MplWidget import MplWidget
-
+from PyQt5 import QtWidgets
 
 class DatabaseWindow(QWidget):
     canvas = None
@@ -18,3 +18,10 @@ class DatabaseWindow(QWidget):
         self.plotWidget = MplWidget(self, width=5, height=4, dpi=100)
         # demo thing:
         self.plotWidget.canvas.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+
+        self.importdbButton = QtWidgets.QPushButton(text="Import DB", parent=self)
+        self.importdbButton.setGeometry(600, 10, 100, 25)
+
+        self.exportdbButton = QtWidgets.QPushButton(text="Export DB", parent=self)
+        self.exportdbButton.setGeometry(600, 40, 100, 25)
+
