40dd8d910f10f44e715a3fcfa27d2c32a856f9c1
welarie <valeriya.iolshina@gmail.com>
2020-11-29 13:59:37 +0300

    Absolut import paths; the application works without PYTHONPATH setting now

--- src/frontend/windows/DatabaseWindow.py
+++ src/frontend/windows/DatabaseWindow.py
@@ -1,8 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QRadioButton, QLabel, QFileDialog, QMessageBox
-from src.frontend.widgets.MplWidget import MplWidget
+from frontend.widgets.MplWidget import MplWidget
 from PyQt5 import QtWidgets
 
-from src.frontend.widgets.QHintCombo import QHintCombo
+from frontend.widgets.QHintCombo import QHintCombo
 
 
 class DatabaseWindow(QWidget):
