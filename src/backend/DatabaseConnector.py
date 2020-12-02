48160fe4728d7e2dad66cb665f1f44727fda0ac5
falvram <wolflammen0@gmail.com>
2020-12-02 20:28:45 +0300

    Create request that creates a bird, request that deletes all nodes

--- src/backend/DatabaseConnector.py
+++ src/backend/DatabaseConnector.py
@@ -13,6 +13,23 @@ class DatabaseConnector:
             greeting = session.write_transaction(self._create_and_return_greeting, message)
             print(greeting)
 
+    def get_birds_area(self, kind):
+        with self.driver.session() as session:
+            area = session.write_transaction(self._get_birds_area, kind)
+            print(area)
+
+    def create_bird(self, id__, url, name, latitude, longitude):
+        with self.driver.session() as session:
+            bird = session.write_transaction(self._create_bird, id__, url, name, latitude, longitude)
+
+    def delete_nodes(self):
+        with self.driver.session() as session:
+            session.write_transaction(self._delete_nodes)
+
+    # def delete_nodes(self):
+    #     with self.driver.session() as session:
+    #         session.write_transaction(self._delete_nodes)
+
     def getSpecies(self):
         with self.driver.session() as session:
             result = session.write_transaction(self._get_all_species)
@@ -56,10 +73,10 @@ class DatabaseConnector:
 
     # Get the bird by id
     @staticmethod
-    def _select_by_id(tx, id):
-        req = '''MATCH (a:Bird {Bird_id: $id})
+    def _select_by_id(tx, id_):
+        req = '''MATCH (a:Bird {Bird_id: $id_})
                  RETURN a'''
-        result = tx.run(req, id=id)
+        result = tx.run(req, id_=id_)
         return result.single()
 
     # Get all of these flying creatures
@@ -70,15 +87,46 @@ class DatabaseConnector:
         result = tx.run(req)
         return result
 
+    # Show the birds flying area by its kind
+    @staticmethod
+    def _get_birds_area(tx, kind):
+        req = '''MATCH (a:Bird)-[:Found_at]->(b:Place)
+                 MATCH (:Bird)-[:Is]->(c:Kind)
+                 WHERE (a)-[:Is]->(c) AND c.name = $kind
+                 RETURN a, b'''
+        result = tx.run(req, kind=kind)
+        return result.values()
 
+    # WHERE a = $kind
 
+    @staticmethod
+    def _create_bird(tx, id__, url, name, latitude, longitude):
+        req = '''CREATE (a:Bird {Bird_id: $id__})
+                 CREATE (c:Kind {name: $name})
+                 CREATE (b:File {URL: $url})
+                 CREATE (d:Place {latitude: $latitude, longitude: $longitude})
+                 CREATE (a)-[:Is]->(c)
+                 CREATE (a)-[:Contains]->(b)
+                 CREATE (a)-[:Found_at]->(d)
+        '''
+        return tx.run(req, id__=id__, url=url, name=name, latitude=latitude, longitude=longitude)
 
-
-
+    @staticmethod
+    def _delete_nodes(tx):
+        req = '''MATCH (n) DETACH DELETE n'''
+        result = tx.run(req)
+        return result
 
 
 if __name__ == "__main__":
     greeter = DatabaseConnector("bolt://localhost:7687", "neo4j", "password")
     # greeter.print_greeting("hello, world")
-    print(greeter.getSpecies())
+    # print(greeter.getSpecies())
+    greeter.delete_nodes()
+    print(greeter.create_bird(0, 1, "Грач", 0.1, 0.2))
+    print(greeter.create_bird(1, 1, "Птеродактиль", 0.3, 0.4))
+    print(greeter.create_bird(2, 1, "Соловей", 0.5, 0.6))
+    print(greeter.create_bird(3, 1, "Грач", 0.7, 0.8))
+    print(greeter.get_birds_area("Грач"))
     greeter.close()
+
