a9025b939b43fde39dcbb2886ea306db6eb1df4c
LosevMikhail <mikhail.losev.1998@gmail.com>
2020-12-20 23:18:11 +0300

    Fixed a bug with excess markers

--- src/frontend/widgets/QGoogleMapsMarkerViewWidget.py
+++ src/frontend/widgets/QGoogleMapsMarkerViewWidget.py
@@ -2,10 +2,15 @@ from frontend.widgets.QGoogleMapsWidget.QGoogleMapsWidget import QGoogleMap
 
 
 class QGoogleMapsMarkerViewWidget(QGoogleMap):
+    labels = []
+
     def __init__(self, api_key, parent=None):
         super(QGoogleMapsMarkerViewWidget, self).__init__(api_key, parent)
 
     def showMarkers(self, markersData):
+        for label in self.labels:
+            self.deleteMarker(label)
+        self.labels = [item[0] for item in markersData]
         for item in markersData:
             markerLabel = item[0]
             self.addMarker(markerLabel, item[1], item[2], **dict(
